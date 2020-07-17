ad0367f9b7ac2553d6d62909d94d9abf22d07020
kindlyman343423 <kindlyman343423@gmail.com>
2020-07-17 12:39:12 +0200

    Fix token list update on send to buyer

--- app/src/components/token/TokensOwned.jsx
+++ app/src/components/token/TokensOwned.jsx
@@ -3,39 +3,35 @@ import { ListGroup } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 
 class TokensOwned extends React.Component {
-	state = {
-		dataKeyTokenIds: null
-	};
+	initialState = {
+		tokenIds: []
+	}
 
-	cacheCallTokens(balance) {
-		let dataKeyTokenIds = []
+	state = this.initialState;
+
+	getTokens(balance) {
+		this.setState(this.initialState)
 		for (var i = 0; i < balance; i++) {
-			dataKeyTokenIds.push(
-				this.props.drizzle.contracts.Logistic.methods.tokenOfOwnerByIndex
-				.cacheCall(this.props.drizzleState.accounts[0], i)
-			)
+			this.props.drizzle.contracts.Logistic.methods.tokenOfOwnerByIndex(
+				this.props.drizzleState.accounts[0], i)
+				.call()
+				.then(tokenId => {
+					this.setState({ tokenIds: [tokenId, ...this.state.tokenIds]})
+				})
 		}
-		this.setState({ dataKeyTokenIds })
 	}
 
 	componentDidMount() {
-		this.cacheCallTokens(this.props.balance)
+		this.getTokens(this.props.balance)
 	}
 
-	componentWillReceiveProps(nextProps) {
-		if (this.props.balance !== nextProps.balance) {
-			this.cacheCallTokens(nextProps.balance)
+	componentDidUpdate(prevProps, prevState) {
+		if (this.props.balance !== prevProps.balance) {
+			this.getTokens(this.props.balance)
 		}
 	}
 
-	renderRow = (dataKey, idx) => {
-		let tokenId = this.props.drizzleState.contracts.Logistic
-			.tokenOfOwnerByIndex[dataKey]
-			&& this.props.drizzleState.contracts.Logistic
-				.tokenOfOwnerByIndex[dataKey].value
-
-		if (!tokenId) return null
-
+	renderRow = (tokenId, idx) => {
 		if (this.props.tokenItemComponent) {
 			return (
 				<this.props.tokenItemComponent
@@ -54,13 +50,12 @@ class TokensOwned extends React.Component {
 	}
 
 	render () {
-		if (!this.state.dataKeyTokenIds) return null
-
+		console.log('render');
 		return (
 			<ListGroup>
 				{
-					this.state.dataKeyTokenIds.map((dataKey, idx) => {
-						return this.renderRow(dataKey, idx)
+					this.state.tokenIds.map((tokenId, idx) => {
+						return this.renderRow(tokenId, idx)
 					})
 				}
 			</ListGroup>

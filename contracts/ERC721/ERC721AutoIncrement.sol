36a8a973d429fdba304829b63f31c1b19fd3bf50
kindlyman343423 <kindlyman343423@gmail.com>
2020-08-07 12:48:49 +0200

    Remove SafeMath as it is in ERC721Full

--- contracts/ERC721/ERC721AutoIncrement.sol
+++ contracts/ERC721/ERC721AutoIncrement.sol
@@ -1,12 +1,9 @@
 pragma solidity ^0.5.0;
 
 import "openzeppelin-solidity/contracts/token/ERC721/ERC721Full.sol";
-import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 
 
 contract ERC721AutoIncrement is ERC721Full("LogisticM", "LM") {
-    using SafeMath for uint256;
-
     uint256 internal counter = 0;
 
     function _mint(address to) internal {

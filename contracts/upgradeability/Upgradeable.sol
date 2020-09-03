572fa3b17c95ebad334f03ea4127de73232f9bf4
kindlyman343423 <kindlyman343423@gmail.com>
2020-09-03 10:26:58 +0200

    Change Upgradeable inheritance

--- contracts/upgradeability/Upgradeable.sol
+++ contracts/upgradeability/Upgradeable.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.5.0;
 
-import "./OwnedUpgradeabilityStorage.sol";
+import "./UpgradeabilityStorage.sol";
 
 
 /**
@@ -8,7 +8,7 @@ import "./OwnedUpgradeabilityStorage.sol";
  * @dev This contract holds all the minimum required functionality for a behavior to be upgradeable.
  * This means, required state variables for owned upgradeability purpose and simple initialization validation.
  */
-contract Upgradeable is OwnedUpgradeabilityStorage {
+contract Upgradeable is UpgradeabilityStorage {
     /**
     * @dev Validates the caller is the versions registry.
     * THIS FUNCTION SHOULD BE OVERRIDDEN CALLING SUPER
